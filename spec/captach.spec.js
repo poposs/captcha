8c7524bb3232c105c865334a3264a318ff6cd25b
poposs <57660009@go.buu.ac.th>
2016-10-13 20:48:57 +0700

    Edit func Captcha, Add func Oparator, numberOparator,stringOparator

--- spec/captach.spec.js
+++ spec/captach.spec.js
@@ -1,28 +1,38 @@
+function Oparator(){
+  this.oparator = function(o){
+    var opa = ['+','-'];
+    return opa[o];
+  }
+}
+function numberOparator() {
+  this.numberOparator = function(r){
+    var number = ['0','1','2','3','4','5','6','7','8','9'];
+    return number[r];
+  }
+
+}
+function stringOparator(){
+  this.stringOparator = function(l){
+    var str = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    return str[l];
+  }
+}
 function Captcha() {
-  var i = 0;
-  var k = 0;
-  var j = 0;
-  var numberOparator = ['0','1','2','3','4','5','6','7','8','9'];
-  var stringOparator = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-  var oparator = ['+','-'];
+  var o = new Oparator();
+  var number = new numberOparator();
+  var string = new stringOparator();
 
-  this.echopattern_one = function(){
-    return numberOparator[i]+" "+oparator[j]+" "+stringOparator[k];
+  this.echopattern_one = function(O,R,L){
+    return number.numberOparator(R)+" "+o.oparator(O)+" "+string.stringOparator(L);
   }
 
-  this.addnumber = function(){
-    i++;
-  }
 }
 describe("Captcha()", function() {
   var echo = new Captcha();
   it('it should be "1 + zero"', function() {
-    echo.addnumber();
     expect(echo.echopattern_one()).toBe('1 + zero');
   });
   it('it should be "2 + one"', function() {
-    echo.addnumber();
-    echo.addString();
     expect(echo.echopattern_one()).toBe('2 + one');
   });
 
