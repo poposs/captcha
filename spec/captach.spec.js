4e8ad07a4368cf5391cd666a3a25d35bb15cad07
poposs <57660009@go.buu.ac.th>
2016-10-13 19:58:34 +0700

    Add function Captcha

--- spec/captach.spec.js
+++ spec/captach.spec.js
@@ -1,9 +1,25 @@
+function Captcha() {
+  var i = 0;
+  var k = 0;
+  var j = 0;
+  var numberOparator = ['0','1','2','3','4','5','6','7','8','9'];
+  var stringOparator = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  var oparator = ['+','-'];
+
+  this.echopattern_one = function(){
+    return numberOparator[i]+" "+oparator[j]+" "+stringOparator[k];
+  }
+  
+  this.addnumber = function(){
+    i++;
+  }
+}
 describe("Captcha()", function() {
   var echo = new Captcha();
   it('it should be "1 + zero"', function() {
     echo.addnumber();
     expect(echo.echopattern_one()).toBe('1 + zero');
   });
-  
+
 
 });
